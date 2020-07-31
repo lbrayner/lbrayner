70a9e16118b703a1c295adc301e5968f4882595d
lbrayner <lbrayner@users.noreply.github.com>
2020-07-31 20:40:07 -0300

    Don't need to worry about sending stdout or stderr to /dev/null

--- dwm/.local/share/dwm/autostart_blocking.sh
+++ dwm/.local/share/dwm/autostart_blocking.sh
@@ -4,4 +4,4 @@ hsetroot -cover ~/.local/share/wallpaper/current
 
 ~/.local/share/dwm/scripts/status &
 # Don't run compton if using a virtual frame buffer
-pgrep -f "Xvfb.*${DISPLAY}\b" >/dev/null 2>&1 || compton -b
+pgrep -f "Xvfb.*${DISPLAY}\b" || compton -b
