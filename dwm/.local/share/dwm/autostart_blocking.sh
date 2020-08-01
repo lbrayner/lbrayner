1239494d89f3d54fa7e02baaf84538c98b4b98e0
lbrayner <lbrayner@users.noreply.github.com>
2020-08-01 20:01:23 -0300

    Sourcing autostart_blocking.local.sh

--- dwm/.local/share/dwm/autostart_blocking.sh
+++ dwm/.local/share/dwm/autostart_blocking.sh
@@ -5,3 +5,8 @@ hsetroot -cover ~/.local/share/wallpaper/current
 ~/.local/share/dwm/scripts/status &
 # Don't run compton if using a virtual frame buffer
 pgrep -f "Xvfb.*${DISPLAY}\b" || compton -b
+
+if [ -f ~/.local/share/dwm/autostart_blocking.local.sh ]
+then
+    ~/.local/share/dwm/autostart_blocking.local.sh
+fi
