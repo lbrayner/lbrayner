015a5c97a768bf3deecd43d4e959aa2d65e9fe44
lbrayner <lbrayner@users.noreply.github.com>
2020-08-28 19:47:07 -0300

    Stowable dotfiles (currently broken) and scripts
    Moved some scripts to bin folders. Updated accordingly.

--- dwm/dot-local/share/dwm/autostart_blocking.sh
+++ dwm/dot-local/share/dwm/autostart_blocking.sh
@@ -2,16 +2,14 @@
 
 hsetroot -cover ~/.local/share/wallpaper/current
 
-~/.local/share/dwm/scripts/status &
+status &
 # If not using a virtual frame buffer
-pgrep -f "Xvfb.*${DISPLAY}\b" || ~/.local/share/wm/scripts/autolock &
-
+pgrep -f "Xvfb.*${DISPLAY}\b" || autolock &
 ~/.local/share/wm/scripts/sxhkd
+
 # If not using a virtual frame buffer
 pgrep -f "Xvfb.*${DISPLAY}\b" || compton -b
 espanso restart
 
-if [ -f ~/.local/share/dwm/autostart_blocking.local.sh ]
-then
+[ -x ~/.local/share/dwm/autostart_blocking.local.sh ] && \
     ~/.local/share/dwm/autostart_blocking.local.sh
-fi
