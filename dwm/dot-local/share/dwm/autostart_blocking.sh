cb0f623e6a4cf32809cdaa8a8333c8d212b4ee13
lbrayner <lbrayner@users.noreply.github.com>
2020-12-15 16:29:23 -0300

    picom as a compositor

--- dwm/dot-local/share/dwm/autostart_blocking.sh
+++ dwm/dot-local/share/dwm/autostart_blocking.sh
@@ -9,7 +9,7 @@ pgrep -f "Xvfb.*${DISPLAY}\b" ||  ~/.local/share/wm/scripts/autolock &
 ~/.local/share/wm/scripts/sxhkd
 
 # Run compositor if not using a virtual frame buffer
-pgrep -f "Xvfb.*${DISPLAY}\b" || compton -b
+pgrep -f "Xvfb.*${DISPLAY}\b" || picom -b
 
 [ -x ~/.local/share/dwm/autostart_blocking.local.sh ] && \
     ~/.local/share/dwm/autostart_blocking.local.sh
