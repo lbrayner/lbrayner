34d8d84e78d042a8018836ab73ca771fa824e996
lbrayner <lbrayner@users.noreply.github.com>
2021-01-13 04:26:03 -0300

    Added comments & line breaks

--- dwm/dot-local/share/dwm/autostart_blocking.sh
+++ dwm/dot-local/share/dwm/autostart_blocking.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+# Wallpaper
 hsetroot -cover ~/.local/share/wallpaper/current
 
 # Dwm status
 ~/.local/share/dwm/scripts/dwm_status &
+
 # Autolock display if not using a virtual frame buffer
 pgrep -f "Xvfb.*${DISPLAY}\b" || xautolock -time 10 -locker slock &
+
+# sxhkd
 ~/.local/share/wm/scripts/sxhkd
 
 # Run compositor if not using a virtual frame buffer
