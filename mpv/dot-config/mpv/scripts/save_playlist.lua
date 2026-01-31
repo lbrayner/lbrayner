400f14581fb44f20467372f03b5e3cc426fa59c6
lbrayner <brayner.l@gmail.com>
2026-01-31 13:08:56 -0300

    WIP

---
+++ mpv/dot-config/mpv/scripts/save_playlist.lua
@@ -0,0 +1,30 @@
+local file_loaded, file_loaded_cb
+
+file_loaded_cb = function()
+  print("file-loaded triggered")
+  if not file_loaded then
+    file_loaded = true
+    mp.unregister_event(file_loaded_cb)
+  end
+end
+
+mp.register_event("file-loaded", file_loaded_cb)
+
+local initial_size
+
+mp.observe_property('playlist-count', 'native', function(name, value)
+  -- value 1 is always triggered
+  -- if value == 1 then
+  --   return
+  -- end
+  if not file_loaded then return end
+
+  -- if not initial_size then
+  --   initial_size = value
+  --   return
+  -- end
+
+  -- Perform your action here when the playlist changes
+  print("Playlist changed:", name, value)
+  -- Add your custom logic here
+end)
