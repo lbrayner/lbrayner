b59a20607b054e1d867afdb375dcbbd1b6ae8885
lbrayner <brayner.l@gmail.com>
2026-02-24 14:21:11 -0300

    bindings: jump to playlist start or end

--- mpv/dot-config/mpv/scripts/bindings.lua
+++ mpv/dot-config/mpv/scripts/bindings.lua
@@ -11,6 +11,22 @@ package.path = concat({ package.path, concat({ home, "lib/?.lua" }, "/") }, ";")
 
 local control = require("control")
 
+mp.add_key_binding("Ctrl+f", "playlist_go_to_start", function()
+  local count = mp.get_property_native("playlist-count")
+
+  if count == 1 then return end
+
+  mp.set_property_native("playlist-pos-1", 1) -- Go to playlist start
+end)
+
+mp.add_key_binding("Ctrl+l", "playlist_go_to_end", function()
+  local count = mp.get_property_native("playlist-count")
+
+  if count == 1 then return end
+
+  mp.set_property_native("playlist-pos-1", count) -- Go to playlist end
+end)
+
 mp.add_key_binding("n", "playlist_next_watch_later", function()
   control.playlist_next_watch_later()
 end)
