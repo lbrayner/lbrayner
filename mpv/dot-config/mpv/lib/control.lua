a02231667a187f659de2c4872ac896a9435201f9
lbrayner <brayner.l@gmail.com>
2026-02-24 14:28:06 -0300

    Moved to control

--- mpv/dot-config/mpv/lib/control.lua
+++ mpv/dot-config/mpv/lib/control.lua
@@ -10,6 +10,24 @@ function M.pause_watch_later()
   mp.set_property_native("pause", not is_paused)
 end
 
+function M.playlist_go_to_start()
+  local count = mp.get_property_native("playlist-count")
+
+  if count == 1 then return end
+
+  mp.command("write-watch-later-config")
+  mp.set_property_native("playlist-pos-1", 1) -- Go to playlist start
+end
+
+function M.playlist_go_to_end()
+  local count = mp.get_property_native("playlist-count")
+
+  if count == 1 then return end
+
+  mp.command("write-watch-later-config")
+  mp.set_property_native("playlist-pos-1", count) -- Go to playlist end
+end
+
 function M.playlist_next_watch_later()
   local count = mp.get_property_native("playlist-count")
 
--- mpv/dot-config/mpv/scripts/bindings.lua
+++ mpv/dot-config/mpv/scripts/bindings.lua
@@ -12,19 +12,11 @@ package.path = concat({ package.path, concat({ home, "lib/?.lua" }, "/") }, ";")
 local control = require("control")
 
 mp.add_key_binding("Ctrl+f", "playlist_go_to_start", function()
-  local count = mp.get_property_native("playlist-count")
-
-  if count == 1 then return end
-
-  mp.set_property_native("playlist-pos-1", 1) -- Go to playlist start
+  control.playlist_go_to_start()
 end)
 
 mp.add_key_binding("Ctrl+l", "playlist_go_to_end", function()
-  local count = mp.get_property_native("playlist-count")
-
-  if count == 1 then return end
-
-  mp.set_property_native("playlist-pos-1", count) -- Go to playlist end
+  control.playlist_go_to_end()
 end)
 
 mp.add_key_binding("n", "playlist_next_watch_later", function()
