a95549650cc01e9306503f06ad4701295ee0720a
lbrayner <brayner.l@gmail.com>
2026-07-18 09:46:35 -0300

    File already on tray

--- mpv/dot-config/mpv/lib/tray.lua
+++ mpv/dot-config/mpv/lib/tray.lua
@@ -9,6 +9,12 @@ local tray = {}
 
 function M.add()
   local filename = get_playlist_filename_at_pos(mp.get_property_native("playlist-pos-1"))
+
+  if tray[filename] then
+    print(filename, "already on tray")
+    return
+  end
+
   tray[filename] = true
   mp.set_property_native("user-data/lbrayner/tray/tray", tray)
   print("Added", filename, "to tray")
