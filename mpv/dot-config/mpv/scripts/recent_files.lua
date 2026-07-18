ac041df1edaddf8c4e8fc2bc3e7c00b9bd018613
lbrayner <brayner.l@gmail.com>
2026-07-18 09:10:15 -0300

    Do not add first file

--- mpv/dot-config/mpv/scripts/recent_files.lua
+++ mpv/dot-config/mpv/scripts/recent_files.lua
@@ -4,10 +4,13 @@ local function get_playlist_filename_at_pos(pos)
   return mp.get_property(concat({ "playlist/", pos - 1, "/filename" }))
 end
 
+local file_loaded, file_loaded_cb
 local recent_files
 local recent_files_dir = "/var/tmp/9572cf67-b586-4c68-a7da-7cb904b396b3/recent_files"
 
 mp.register_event("file-loaded", function()
+  if not file_loaded then return end
+
   if not recent_files then
     os.execute(concat{ "test -d ", recent_files_dir, " || mkdir -p ", recent_files_dir })
 
@@ -23,3 +26,13 @@ mp.register_event("file-loaded", function()
   file:close()
   print("Appended to", recent_files)
 end)
+
+file_loaded_cb = function()
+  print("file-loaded triggered")
+  if not file_loaded then
+    file_loaded = true
+    mp.unregister_event(file_loaded_cb)
+  end
+end
+
+mp.register_event("file-loaded", file_loaded_cb)
