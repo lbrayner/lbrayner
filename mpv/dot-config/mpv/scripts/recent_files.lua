59d543792268882e3b0eb06806812796a6886eb8
lbrayner <brayner.l@gmail.com>
2026-07-22 10:28:56 -0300

    Deduplicated

--- mpv/dot-config/mpv/scripts/recent_files.lua
+++ mpv/dot-config/mpv/scripts/recent_files.lua
@@ -5,25 +5,32 @@ local function get_playlist_filename_at_pos(pos)
 end
 
 local file_loaded, file_loaded_cb
-local recent_files
+local recent_files, recent_files_filename = {}
 local recent_files_dir = "/var/tmp/9572cf67-b586-4c68-a7da-7cb904b396b3/recent_files"
 
 mp.register_event("file-loaded", function()
   if not file_loaded then return end
 
-  if not recent_files then
+  if not recent_files_filename then
     os.execute(concat{ "test -d ", recent_files_dir, " || mkdir -p ", recent_files_dir })
 
     local tmpname = os.tmpname():match("([^/\\]+)$")
-    recent_files = concat({ recent_files_dir, "/", "recent_files_", tmpname, ".m3u" })
+    recent_files_filename = concat({ recent_files_dir, "/", "recent_files_", tmpname, ".m3u" })
   end
 
   local filename = get_playlist_filename_at_pos(mp.get_property_native("playlist-pos-1"))
 
-  local file = io.open(recent_files, "a")
+  if recent_files[filename] then
+    print("Already present:", filename)
+    return
+  end
+
+  recent_files[filename] = true
+
+  local file = io.open(recent_files_filename, "a")
   file:write(concat({ filename, "\n" }))
   file:close()
-  print("Appended to", recent_files)
+  print("Appended to", recent_files_filename)
 end)
 
 file_loaded_cb = function()
