0de2fe62613cbef60903a43deb2afbc227edf722
lbrayner <brayner.l@gmail.com>
2026-07-25 08:16:20 -0300

    File name has to be a word

--- mpv/dot-config/mpv/common/lbrayner/lib/utils.lua
+++ mpv/dot-config/mpv/common/lbrayner/lib/utils.lua
@@ -5,7 +5,7 @@ local ipc_name
 function M.get_ipc_name()
   if ipc_name ~= nil then return ipc_name end
 
-  ipc_name = mp.get_property("input-ipc-server"):match("([%w_]+)$")
+  ipc_name = mp.get_property("input-ipc-server"):match("/?([%w_]+)$")
 
   if not ipc_name then
     ipc_name = false
