4c36d018a0451f281a070bae8f4d9aba224aceac
lbrayner <brayner.l@gmail.com>
2026-07-26 19:56:08 -0300

    Backup function to be used when deleting, user-data state

--- mpv/dot-config/mpv/scripts/lbrayner/playlist_jump_ring.lua
+++ mpv/dot-config/mpv/scripts/lbrayner/playlist_jump_ring.lua
@@ -2,41 +2,75 @@ local function log(...)
   print("[playlist_jump_ring]", ...)
 end
 
-local concat = table.concat
-
-local function get_playlist_filename_at_pos(pos)
-  return mp.get_property(concat({ "playlist/", pos - 1, "/filename" }))
-end
-
-local utils = require("lbrayner/lib/utils")
-
 local PLAYLIST_JUMP_RING = (
   "user-data/lbrayner/playlist_jump_ring/playlist_jump_ring"
 )
-local jump_ring, jump_ring_index, jump_ring_filename = {}, {}
-local jump_ring_dir = "/var/tmp/9572cf67-b586-4c68-a7da-7cb904b396b3/backup/playlist_jump_ring"
+local PLAYLIST_JUMP_RING_INDEX = (
+  "user-data/lbrayner/playlist_jump_ring/playlist_jump_ring_index"
+)
+local backup_dir, created_backup_dir = (
+  "/var/tmp/9572cf67-b586-4c68-a7da-7cb904b396b3/backup/playlist_jump_ring"
+)
+local concat = table.concat
+local jump_ring_dir, jump_ring_filename = (
+  "/var/tmp/9572cf67-b586-4c68-a7da-7cb904b396b3/playlist_jump_ring"
+)
+
+local utils = require("lbrayner/lib/utils")
+
+local function backup()
+  local ipc_name = utils.get_ipc_name()
+
+  if not ipc_name then return end
+
+  if not created_backup_dir then
+    os.execute(concat{ "test -d ", backup_dir, " || mkdir -p ", backup_dir })
+    created_backup_dir = true
+  end
+
+  local tmpname = os.tmpname():match("([%w_]+)$")
+  local backup_path = concat({
+    backup_dir, "/", ipc_name, "-playlist_jump_ring-", tmpname
+  })
+
+  local file = io.open(backup_path, "w")
+
+  for _, item in ipairs(jump_ring) do
+    file:write(concat({ item, "\n" }))
+  end
+
+  file:close()
+  log("Backed up Playlist Jump Ring to", playlist_name)
+end
 
 mp.register_event("file-loaded", function()
   if not utils.is_file_loaded() then return end
 
   if not jump_ring_filename then
     os.execute(concat{ "test -d ", jump_ring_dir, " || mkdir -p ", jump_ring_dir })
-
-    local tmpname, ipc_name = os.tmpname():match("([^/\\]+)$"), utils.get_ipc_name() or ""
-    jump_ring_filename = concat({
-      jump_ring_dir, "/", ipc_name, "-playlist_jump_ring-", tmpname, ".m3u"
-    })
+    jump_ring_filename = concat({ jump_ring_dir, "/", ipc_name })
   end
 
-  local filename = get_playlist_filename_at_pos(mp.get_property_native("playlist-pos-1"))
+  local filename = mp.get_property(concat({
+    "playlist/", mp.get_property("playlist-pos"), "/filename"
+  }))
+
+  local jump_ring_index = mp.get_property_native(
+    PLAYLIST_JUMP_RING_INDEX
+  ) or {}
 
   if jump_ring_index[filename] then
     log("Already present:", filename)
     return
   end
 
+  local jump_ring = mp.get_property_native(
+    PLAYLIST_JUMP_RING
+  ) or {}
+
   jump_ring_index[filename] = true
   table.insert(jump_ring, filename)
+  mp.set_property_native(PLAYLIST_JUMP_RING_INDEX, jump_ring_index)
   mp.set_property_native(PLAYLIST_JUMP_RING, jump_ring)
 
   local file = io.open(jump_ring_filename, "a")
