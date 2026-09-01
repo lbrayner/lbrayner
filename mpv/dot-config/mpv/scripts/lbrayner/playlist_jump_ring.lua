1dfb01e01d1cc8904f5ac3bb8e768e6641ade798
lbrayner <brayner.l@gmail.com>
2026-09-01 15:42:05 -0300

    playlist_jump_ring: add first  file

--- mpv/dot-config/mpv/scripts/lbrayner/playlist_jump_ring.lua
+++ mpv/dot-config/mpv/scripts/lbrayner/playlist_jump_ring.lua
@@ -4,11 +4,8 @@ end
 
 local concat = table.concat
 local playlist_jump_ring = require("lbrayner/lib/playlist_jump_ring")
-local utils = require("lbrayner/lib/utils")
 
 mp.register_event("file-loaded", function()
-  if not utils.is_file_loaded() then return end
-
   local filename = mp.get_property(concat({
     "playlist/", mp.get_property("playlist-pos"), "/filename"
   }))
